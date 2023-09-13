3d6146e45c3fee9806795ddb3d96e2e55d6e2f74
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-09-12 22:22:29 -0300

    Update USO_DE_SELECT_01.sql
    adição de novos exercicios feitos

--- USO_DE_SELECT_01.sql
+++ USO_DE_SELECT_01.sql
@@ -38,3 +38,32 @@ FROM departamento ORDER BY nome_depto ASC;
 -- 11) Selecione todos os projetos em ordem alfabética descendente com base em seus nomes.
 SELECT sigla_projeto, nome_projeto
 FROM projeto ORDER BY nome_projeto DESC;
+
+-- 12)  Considere que a tabela funcionário precisa ser alterada, será necessário
+adicionar a coluna data_admissao (tipo date), salario (tipo decimal com até duas casas de precisão) 
+ALTER TABLE funcionario ADD COLUMN data_admissao DATE,
+ADD COLUMN salario DECIMAL(10, 2); 
+ 
+-- 13) Considere que a tabela projeto também precisa ser alterada, será necessário adicionar a coluna duração (tipo numérico) deve representar em meses a duração de um projeto.
+ALTER TABLE projeto ADD COLUMN duracao INT;
+
+-- 14)a) Para garantir a consistência dos dados você deve fazer os inserts (de data de admissão e salário) nas duplas já existentes. 
+
+UPDATE funcionario SET data_admissao = '2017-09-21', salario = 4000.00 WHERE codigo_funcionario = 100; 
+UPDATE funcionario SET data_admissao = '2019-05-29', salario = 3000.00 WHERE codigo_funcionario = 101; 
+UPDATE funcionario SET data_admissao = '2015-01-01', salario = 6500.00 WHERE codigo_funcionario = 102; 
+UPDATE funcionario SET data_admissao = '2020-11-15', salario = 3400.00 WHERE codigo_funcionario = 103; 
+UPDATE funcionario SET data_admissao = '2021-07-10', salario = 3600.00 WHERE codigo_funcionario = 104;
+UPDATE funcionario SET data_admissao = '2018-02-01', salario = 3000.00 WHERE codigo_funcionario = 105;
+UPDATE funcionario SET data_admissao = '2018-02-01', salario = 4100.00 WHERE codigo_funcionario = 106;
+UPDATE funcionario SET data_admissao = '2020-03-12', salario = 5200.00 WHERE codigo_funcionario = 107;
+UPDATE funcionario SET data_admissao = '2020-03-18', salario = 4900.00 WHERE codigo_funcionario = 108;
+UPDATE funcionario SET data_admissao = '2020-03-02', salario = 3700.00 WHERE codigo_funcionario = 109;
+UPDATE funcionario SET data_admissao = '2021-04-22', salario = 4600.00 WHERE codigo_funcionario = 110;
+UPDATE funcionario SET data_admissao = '2021-04-22', salario = 4600.00 WHERE codigo_funcionario = 111;
+UPDATE funcionario SET data_admissao = '2015-01-01', salario = 6500.00 WHERE codigo_funcionario = 112;
+
+-- 14) b) Agora faça as alterações necessárias para deixar os campos data_admissao e salario como itens obrigatórios para futuros inserts.
+ALTER TABLE funcionario
+MODIFY COLUMN data_admissao DATE NOT NULL,
+MODIFY COLUMN salario DECIMAL(10, 2) NOT NULL;
